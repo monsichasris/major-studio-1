2fd62edbabb3fc02a43490fd1acf8dc5e47c53ea
hushbrush <harshitachakravadhanula@gmail.com>
2024-10-12 17:47:32 -0400

    checking if no conflict

--- project02/main.js
+++ project02/main.js
@@ -1,1 +1,13 @@
 //refresh
+// Parse the JSON string into a JavaScript object
+// Parse the JSON string into a JavaScript object
+ // Load the JSON data
+ d3.json('data_women.json').then(function(data) {
+    // Log the data to check its structure
+    console.log(data);
+
+   
+  }).catch(function(error) {
+    console.error('Error loading the JSON data:', error);
+  });
+//for (i=0; i<)
