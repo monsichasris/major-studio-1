1e6422366d0fba66dbebaa52b14357f93dc4b6b6
monsichasris <monsicha.srisuantang@gmail.com>
2025-04-06 17:38:21 -0400

    add env

--- lab02_smithsonian_api/ex1_api_fetch_search/fetch_search.js
+++ lab02_smithsonian_api/ex1_api_fetch_search/fetch_search.js
@@ -2,7 +2,7 @@
 // check API documentation for search here: http://edan.si.edu/openaccess/apidocs/#api-search-search
 
 // put your API key here;
-const apiKey = "OBP6w8aw6IMti0Ip1efxd3z10OzeDyxqXVcRSCOK";
+const apiKey = process.env.API_KEY;
 
 // search base URL
 const searchBaseURL =
@@ -15,20 +15,19 @@ const search = `topic:"Cats" AND online_media_type:"Images"`;
 
 //https://collections.si.edu/search/results.htm?view=grid&fq=online_media_type%3A%22Images%22&q=dog&media.CC0=true&gfq=CSILP_4
 
-
 // search: fetches an array of terms based on term category
 function fetchSearchData(searchTerm) {
   let url = searchBaseURL + "?api_key=" + apiKey + "&q=" + searchTerm;
   console.log(url);
   window
     .fetch(url)
-    .then(res => res.json())
-    .then(data => {
+    .then((res) => res.json())
+    .then((data) => {
       console.log(data);
     })
-    .catch(error => {
+    .catch((error) => {
       console.log(error);
-    })
+    });
 }
 
 fetchSearchData(search);
